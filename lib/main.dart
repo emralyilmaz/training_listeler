3345ee546cba1846c19f4efe934a55b80ad6b47c
Emral Yılmaz <emralyilmaz@Emrals-MacBook-Air.local>
2020-07-26 11:57:34 +0300

    training_gridView

--- lib/main.dart
+++ lib/main.dart
@@ -29,114 +29,16 @@ class _FormWidgetState extends State<FormWidget> {
       appBar: AppBar(
         title: Text("ListTile"),
       ),
-      body: Align(
-        alignment: Alignment.bottomCenter,
-        child: Container(
-          margin: EdgeInsets.symmetric(
-            vertical: 1.0,
-          ),
-          height: 50,
-          child: ListView(
-            scrollDirection: Axis.horizontal,
-            children: <Widget>[
-              Container(
-                width: 160.0,
-                color: Colors.red,
-                child: GestureDetector(
-                  onTap: () {
-                    print("telefon tıklandı");
-                  },
-                  child: Center(
-                    child: Stack(
-                      children: <Widget>[
-                        ListTile(
-                          leading: Icon(Icons.phone),
-                          title: Text("Telefon"),
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-              Container(
-                width: 160.0,
-                color: Colors.blue,
-                child: GestureDetector(
-                  onTap: () {
-                    print("harita tıklandı");
-                  },
-                  child: Center(
-                    child: Stack(
-                      children: <Widget>[
-                        ListTile(
-                          leading: Icon(Icons.map),
-                          title: Text("harita"),
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-              Container(
-                width: 160.0,
-                color: Colors.green,
-                child: GestureDetector(
-                  onTap: () {
-                    print("kamera tıklandı");
-                  },
-                  child: Center(
-                    child: Stack(
-                      children: <Widget>[
-                        ListTile(
-                          leading: Icon(Icons.camera),
-                          title: Text("kamera"),
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-              Container(
-                width: 160.0,
-                color: Colors.yellow,
-                child: GestureDetector(
-                  onTap: () {
-                    print("ayarlar tıklandı");
-                  },
-                  child: Center(
-                    child: Stack(
-                      children: <Widget>[
-                        ListTile(
-                          leading: Icon(Icons.settings),
-                          title: Text("ayarlar"),
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-              Container(
-                width: 160.0,
-                color: Colors.grey,
-                child: GestureDetector(
-                  onTap: () {
-                    print("rehber tıklandı");
-                  },
-                  child: Center(
-                    child: Stack(
-                      children: <Widget>[
-                        ListTile(
-                          leading: Icon(Icons.person),
-                          title: Text("rehber"),
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
+      body: GridView.count(
+        crossAxisCount: 3,
+        children: List.generate(1000, (index) {
+          return Center(
+            child: Text(
+              "veri $index",
+              style: Theme.of(context).textTheme.title,
+            ),
+          );
+        }),
       ),
     );
   }
