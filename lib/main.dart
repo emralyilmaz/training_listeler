8d818603f56d4312dc761c06b0a85ad70c1ed671
Emral Yılmaz <emralyilmaz@Emrals-MacBook-Air.local>
2020-07-26 11:47:06 +0300

    yatay_liste

--- lib/main.dart
+++ lib/main.dart
@@ -23,19 +23,121 @@ class FormWidget extends StatefulWidget {
 }
 
 class _FormWidgetState extends State<FormWidget> {
-  final urunler = List<String>.generate(1000, (i) => "ürün ${i + 1}");
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("ListTile"),
+      appBar: AppBar(
+        title: Text("ListTile"),
+      ),
+      body: Align(
+        alignment: Alignment.bottomCenter,
+        child: Container(
+          margin: EdgeInsets.symmetric(
+            vertical: 1.0,
+          ),
+          height: 50,
+          child: ListView(
+            scrollDirection: Axis.horizontal,
+            children: <Widget>[
+              Container(
+                width: 160.0,
+                color: Colors.red,
+                child: GestureDetector(
+                  onTap: () {
+                    print("telefon tıklandı");
+                  },
+                  child: Center(
+                    child: Stack(
+                      children: <Widget>[
+                        ListTile(
+                          leading: Icon(Icons.phone),
+                          title: Text("Telefon"),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                width: 160.0,
+                color: Colors.blue,
+                child: GestureDetector(
+                  onTap: () {
+                    print("harita tıklandı");
+                  },
+                  child: Center(
+                    child: Stack(
+                      children: <Widget>[
+                        ListTile(
+                          leading: Icon(Icons.map),
+                          title: Text("harita"),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                width: 160.0,
+                color: Colors.green,
+                child: GestureDetector(
+                  onTap: () {
+                    print("kamera tıklandı");
+                  },
+                  child: Center(
+                    child: Stack(
+                      children: <Widget>[
+                        ListTile(
+                          leading: Icon(Icons.camera),
+                          title: Text("kamera"),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                width: 160.0,
+                color: Colors.yellow,
+                child: GestureDetector(
+                  onTap: () {
+                    print("ayarlar tıklandı");
+                  },
+                  child: Center(
+                    child: Stack(
+                      children: <Widget>[
+                        ListTile(
+                          leading: Icon(Icons.settings),
+                          title: Text("ayarlar"),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                width: 160.0,
+                color: Colors.grey,
+                child: GestureDetector(
+                  onTap: () {
+                    print("rehber tıklandı");
+                  },
+                  child: Center(
+                    child: Stack(
+                      children: <Widget>[
+                        ListTile(
+                          leading: Icon(Icons.person),
+                          title: Text("rehber"),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
-        body: ListView.builder(
-            itemCount: urunler.length,
-            itemBuilder: (context, index) {
-              return ListTile(
-                title: Text("${urunler[index]}"),
-              );
-            }));
+      ),
+    );
   }
 }
